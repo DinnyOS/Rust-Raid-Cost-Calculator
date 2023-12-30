f6276aec1159bfa50f919e4a51ca8b4be97fab71
DinnyOS <155203669+DinnyOS@users.noreply.github.com>
2023-12-30 01:30:01 +0100

    Add files via upload

---
+++ RustCalc.py
@@ -0,0 +1,18 @@
+# foundation data
+
+sulfur_costs = {"wooden_wall": 1400,  # 1 Rocket + 10 Rifle Ammunition
+                "stone_wall": 5600,  # 4 Rockets
+                "sheet_metal_wall": 8400,  # 3 C4's + 75 Explosive Rifle Ammo
+                "armored_wall": 16150,  # 7 C4's + 30 Explosive Rifle Ammo
+                "wooden_door": 450,  # 18 Explosive Rifle Ammo
+                "sheet_metal_door": 1600,  # 63 Explosive Rifle Ammo
+                "garage_door": 3600,  # 1 C4 + 1 Rocket
+                "armored_door": 4400,  # 2 C4's
+                "armored_double_door": 5200,  # 2 C4's + 31 Explosive Rifle Ammo
+                "auto_turret": 600,  # 3 High Velocity Rockets
+                "workbench_3": 4400,  # 2 C4's
+                "external_stone_wall": 4400,  # 2 C4's
+                "embrasure": 5600,  # 4 Rockets
+                "ladder_hatch": 1600}  # 63 Explosive Rifle Ammo
+
+
